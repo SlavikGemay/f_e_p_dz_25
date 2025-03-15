446968ee2152cf8362a4a7b689bd406b3c4a2209
SlavikGemay <gemayit@gmail.com>
2025-03-15 17:57:03 +0200

    dz 25

---
+++ js/index.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const toDoList = document.querySelector('.js--todos-wrapper');
+
+if (localStorage.getItem('activeClick')) {
+    toDoList.innerHTML = localStorage.getItem('activeClick');
+}
+
+document.querySelector('form button').onclick = (event) => {
+    event.preventDefault();
+
+    const addTask = document.querySelector('.js--form__input');
+    addTask.value;   // Мария, мне кажется эта строка лишняя?
+
+    if (addTask.value?.trim()) {
+        const itemOfList = document.createElement('li');
+        itemOfList.classList.add('todo-item');
+
+        const someForListOne = document.createElement('input');
+        someForListOne.type = 'checkbox';
+
+        const someForListTwo = document.createElement('span');
+        someForListTwo.textContent = addTask.value;
+        someForListTwo.classList.add('todo-item__description');
+
+        const someForListThree = document.createElement('button');
+        someForListThree.textContent = 'Видалити';
+        someForListThree.classList.add('todo-item__delete');
+
+        itemOfList.append(someForListOne, someForListTwo, someForListThree);
+        toDoList.append(itemOfList);
+
+        localStorage.setItem('activeClick', toDoList.innerHTML);
+    }
+};
+
+document.querySelector('.js--todos-wrapper').addEventListener('click', (event) => {
+    if (event.target.nodeName === 'BUTTON') {
+        event.target.parentElement.remove();
+
+        localStorage.setItem('activeClick', toDoList.innerHTML);
+    }
+});
